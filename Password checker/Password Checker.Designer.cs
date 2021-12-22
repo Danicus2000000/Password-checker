98f544667d56a58888589a157c133db255a65b79
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2021-12-22 05:20:35 +0000

    Add project files.

---
+++ Password checker/Password Checker.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Password_checker
+{
+    partial class password_checker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernamentry_txt = new System.Windows.Forms.TextBox();
+            this.title_lbl = new System.Windows.Forms.Label();
+            this.confirm_btn = new System.Windows.Forms.Button();
+            this.question_lbl = new System.Windows.Forms.Label();
+            this.passwordentry_txt = new System.Windows.Forms.TextBox();
+            this.password_lbl = new System.Windows.Forms.Label();
+            this.username_lbl = new System.Windows.Forms.Label();
+            this.passwordstrength_bar = new System.Windows.Forms.ProgressBar();
+            this.description_lbl = new System.Windows.Forms.Label();
+            this.advice_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // usernamentry_txt
+            // 
+            this.usernamentry_txt.Location = new System.Drawing.Point(90, 50);
+            this.usernamentry_txt.Margin = new System.Windows.Forms.Padding(4);
+            this.usernamentry_txt.Name = "usernamentry_txt";
+            this.usernamentry_txt.Size = new System.Drawing.Size(293, 22);
+            this.usernamentry_txt.TabIndex = 0;
+            // 
+            // title_lbl
+            // 
+            this.title_lbl.AutoSize = true;
+            this.title_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_lbl.Location = new System.Drawing.Point(1, 2);
+            this.title_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.title_lbl.Name = "title_lbl";
+            this.title_lbl.Size = new System.Drawing.Size(177, 25);
+            this.title_lbl.TabIndex = 1;
+            this.title_lbl.Text = "Password Checker";
+            // 
+            // confirm_btn
+            // 
+            this.confirm_btn.Location = new System.Drawing.Point(165, 106);
+            this.confirm_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.confirm_btn.Name = "confirm_btn";
+            this.confirm_btn.Size = new System.Drawing.Size(100, 28);
+            this.confirm_btn.TabIndex = 2;
+            this.confirm_btn.Text = "Enter";
+            this.confirm_btn.UseVisualStyleBackColor = true;
+            this.confirm_btn.Click += new System.EventHandler(this.confirm_btn_Click);
+            // 
+            // question_lbl
+            // 
+            this.question_lbl.AutoSize = true;
+            this.question_lbl.Location = new System.Drawing.Point(51, 27);
+            this.question_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.question_lbl.Name = "question_lbl";
+            this.question_lbl.Size = new System.Drawing.Size(283, 17);
+            this.question_lbl.TabIndex = 3;
+            this.question_lbl.Text = "Please enter your username and password.";
+            // 
+            // passwordentry_txt
+            // 
+            this.passwordentry_txt.Location = new System.Drawing.Point(90, 76);
+            this.passwordentry_txt.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordentry_txt.Name = "passwordentry_txt";
+            this.passwordentry_txt.Size = new System.Drawing.Size(293, 22);
+            this.passwordentry_txt.TabIndex = 4;
+            this.passwordentry_txt.UseSystemPasswordChar = true;
+            // 
+            // password_lbl
+            // 
+            this.password_lbl.AutoSize = true;
+            this.password_lbl.Location = new System.Drawing.Point(9, 79);
+            this.password_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.password_lbl.Name = "password_lbl";
+            this.password_lbl.Size = new System.Drawing.Size(73, 17);
+            this.password_lbl.TabIndex = 5;
+            this.password_lbl.Text = "Password:";
+            // 
+            // username_lbl
+            // 
+            this.username_lbl.AutoSize = true;
+            this.username_lbl.Location = new System.Drawing.Point(9, 50);
+            this.username_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.username_lbl.Name = "username_lbl";
+            this.username_lbl.Size = new System.Drawing.Size(77, 17);
+            this.username_lbl.TabIndex = 6;
+            this.username_lbl.Text = "Username:";
+            // 
+            // passwordstrength_bar
+            // 
+            this.passwordstrength_bar.Enabled = false;
+            this.passwordstrength_bar.Location = new System.Drawing.Point(12, 143);
+            this.passwordstrength_bar.Name = "passwordstrength_bar";
+            this.passwordstrength_bar.Size = new System.Drawing.Size(417, 23);
+            this.passwordstrength_bar.TabIndex = 7;
+            this.passwordstrength_bar.Visible = false;
+            // 
+            // description_lbl
+            // 
+            this.description_lbl.AutoSize = true;
+            this.description_lbl.Enabled = false;
+            this.description_lbl.Location = new System.Drawing.Point(9, 123);
+            this.description_lbl.Name = "description_lbl";
+            this.description_lbl.Size = new System.Drawing.Size(129, 17);
+            this.description_lbl.TabIndex = 8;
+            this.description_lbl.Text = "Password strength:";
+            this.description_lbl.Visible = false;
+            // 
+            // advice_lbl
+            // 
+            this.advice_lbl.AutoSize = true;
+            this.advice_lbl.Enabled = false;
+            this.advice_lbl.Location = new System.Drawing.Point(9, 169);
+            this.advice_lbl.Name = "advice_lbl";
+            this.advice_lbl.Size = new System.Drawing.Size(54, 17);
+            this.advice_lbl.TabIndex = 9;
+            this.advice_lbl.Text = "Advice:";
+            this.advice_lbl.Visible = false;
+            // 
+            // password_checker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 291);
+            this.Controls.Add(this.advice_lbl);
+            this.Controls.Add(this.description_lbl);
+            this.Controls.Add(this.passwordstrength_bar);
+            this.Controls.Add(this.username_lbl);
+            this.Controls.Add(this.password_lbl);
+            this.Controls.Add(this.passwordentry_txt);
+            this.Controls.Add(this.question_lbl);
+            this.Controls.Add(this.confirm_btn);
+            this.Controls.Add(this.title_lbl);
+            this.Controls.Add(this.usernamentry_txt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "password_checker";
+            this.ShowIcon = false;
+            this.Text = "Password Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernamentry_txt;
+        private System.Windows.Forms.Label title_lbl;
+        private System.Windows.Forms.Button confirm_btn;
+        private System.Windows.Forms.Label question_lbl;
+        private System.Windows.Forms.TextBox passwordentry_txt;
+        private System.Windows.Forms.Label password_lbl;
+        private System.Windows.Forms.Label username_lbl;
+        private System.Windows.Forms.ProgressBar passwordstrength_bar;
+        private System.Windows.Forms.Label description_lbl;
+        private System.Windows.Forms.Label advice_lbl;
+    }
+}
+
---
+++ Password checker/Password Checker.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_checker
+{
+    public partial class password_checker : Form
+    {
+        public password_checker()
+        {
+            InitializeComponent();
+        }
+        private void confirm_btn_Click(object sender, EventArgs e)//when you click the confirm button
+        {
+            if (usernamentry_txt.Text != "" && passwordentry_txt.Text!="")//if the boxes are not empty
+            {
+                advice_lbl.Text = "Advice:";//resets the advice label
+                char[] password = passwordentry_txt.Text.ToArray();//converts the password to an array of the individual characters
+                string[] username = usernamentry_txt.Text.Split(' ');//converts the username to an array of the individual words
+                bool gotcap = false;//stores whether password has a capital
+                bool gotnoncap = false;//stores whether the password has a lower case letter
+                bool gotnumber = false;//stores whether the password has a number
+                bool gotsymbol = false;//stores whether the password has a symbol
+                bool uses_part_of_username = false;//checks whether the password uses a word in the username
+                int score = 0;//tracks the score as a percentage for progress bar
+                foreach (string element in username)//loops through each word in the username
+                {
+                    if (passwordentry_txt.Text.Contains(element))//checks whether the password contains the word
+                    {
+                        uses_part_of_username = true;//sets to true if it does
+                    }
+                }
+                foreach (char element in password)//loops through each letter in the password
+                {
+                    if (char.IsUpper(element))//if the letter is uppercase
+                    {
+                        gotcap = true;//sets to true
+                    }
+                    if (char.IsLower(element))//if the letter is lower case
+                    {
+                        gotnoncap = true;//sets to true
+                    }
+                    if (char.IsNumber(element))//if the character is a number
+                    {
+                        gotnumber = true;//sets to true
+                    }
+                    if (char.IsSymbol(element))//if the character is a symbol
+                    {
+                        gotsymbol = true;//sets a true
+                    }
+                }
+                if (gotcap == true)//if the word has got a capital
+                {
+                    score += 20; //adds 20% strength to password
+                }
+                else if (gotcap == false)//if the password has no capital
+                {
+                    advice_lbl.Text += "\n-Add a capital letter to your password.";//adds capital letter reuest to advice
+                }
+                if (gotnoncap == true)//if the word has a lower case
+                {
+                    score += 20;//adds 20% score
+                }
+                else if (gotnoncap == false)//if it doesn't have a lower case letter
+                {
+                    advice_lbl.Text += "\n-Add a lower case letter to your password.";//adds lower case letter to advice list
+                }
+                if (gotnumber == true)//if the word has a number
+                {
+                    score += 20;//adds 20% to score
+                }
+                else if (gotnumber == false)//if the password hasn't got a number
+                {
+                    advice_lbl.Text += "\n-Add a number to your password.";//adds add number to advice
+                }
+                if (gotsymbol == true)//if the password has a symbol
+                {
+                    score += 20;//adds 20% to score
+                }
+                else if (gotsymbol == false)//if password has no symbol
+                {
+                    advice_lbl.Text += "\n-Add a symbol to your password.";//adds symbol to advice
+                }
+                if (uses_part_of_username == false)//if the password doesn't use a word out of the username
+                {
+                    score += 20;//adds 20% to the score
+                }
+                else if (uses_part_of_username == true)//if the password uses a word from the username
+                {
+                    advice_lbl.Text += "\n-Don't use words from your username in your password";//adds to advice list
+                }
+                //shows the password stats aread
+                passwordstrength_bar.Visible = true;
+                passwordstrength_bar.Enabled = true;
+                passwordstrength_bar.Value = score;
+                description_lbl.Visible = true;
+                description_lbl.Enabled = true;
+                advice_lbl.Visible = true;
+                advice_lbl.Enabled = true;
+                if (passwordstrength_bar.Value == 100)//if the password is at 100% strength
+                {
+                    advice_lbl.Text = "You've got a great password!";//turns advice label into a congratulations.
+                }
+            }
+            else//if one of the textboxes are empty
+            {
+                MessageBox.Show("One or more fields are empty!", "Empty field(s)");//display message to warn user
+            }
+        }
+    }
+}
